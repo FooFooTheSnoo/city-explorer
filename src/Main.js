be91e967f0df53a0222949e45b5008502d4f0f0e
Joshua Hahn <114379383+FooFooTheSnoo@users.noreply.github.com>
2023-05-18 20:38:20 -0700

    added cache

--- src/Main.js
+++ src/Main.js
@@ -40,6 +40,7 @@ class Main extends React.Component {
       const lat = response.data[0].lat;
       const lon = response.data[0].lon;
       let weatherUrl = `${localToken}weather?lat=${lat}&lon=${lon}&searchQuery=${this.state.city}`
+      // console.log(weatherUrl);
       const weatherResponse = await axios.get(weatherUrl)
       let movieUrl = `${localToken}movie?film=${this.state.city}`
       const movieResponse = await axios.get(movieUrl)
